0d8465804e15b7be9dba9afeb6ffdd9fc0c92d07
Pablode <private@pablode.com>
2015-10-11 15:19:36 +0200

    Replaced "OpenPrinter" references in BuildInfo with "OpenFM"

--- src/main/java/pcl/OpenFM/BuildInfo.java
+++ src/main/java/pcl/OpenFM/BuildInfo.java
@@ -7,8 +7,8 @@ package pcl.OpenFM;
  * @author AfterLifeLochie, stolen from LanteaCraft, another fine PC-Logix Minecraft mod.
  */
 public class BuildInfo {
-	public static final String modName = "OpenPrinter";
-	public static final String modID = "openprinter";
+	public static final String modName = "OpenFM";
+	public static final String modID = "openfm";
 
 	public static final String versionNumber = "@VERSION@";
 	public static final String buildNumber = "@BUILD@";
