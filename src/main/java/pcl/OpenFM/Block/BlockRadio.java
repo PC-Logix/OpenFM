d5018cce814e30627b06695a74fd523e45b51b1e
Caitlyn <Michiyo.Ravencroft@gmail.com>
2015-10-12 14:43:38 -0500

    Fix NPE opening UI on upgraded TEs.

--- src/main/java/pcl/OpenFM/Block/BlockRadio.java
+++ src/main/java/pcl/OpenFM/Block/BlockRadio.java
@@ -101,7 +101,11 @@ public class BlockRadio extends Block implements ITileEntityProvider {
 		if (side == Side.CLIENT)
 		{
 			TileEntityRadio ter = (TileEntityRadio)par1World.getTileEntity(par2, par3, par4);
-			if (MinecraftServer.getServer().getConfigurationManager().func_152596_g(par5EntityPlayer.getGameProfile()) || !ter.isLocked || ter.owner.equals(par5EntityPlayer.getUniqueID().toString())) {
+			if (ter.owner != null) {
+				if (MinecraftServer.getServer().getConfigurationManager().func_152596_g(par5EntityPlayer.getGameProfile()) || !ter.isLocked || ter.owner.equals(par5EntityPlayer.getUniqueID().toString())) {
+					openGUI(par1World, par2, par3, par4);
+				}
+			} else {
 				openGUI(par1World, par2, par3, par4);
 			}
 		}
