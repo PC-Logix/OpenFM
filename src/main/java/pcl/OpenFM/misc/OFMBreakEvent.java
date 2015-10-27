fb7410276e7a71a55dd6e39b6910db7e41b49b47
Caitlyn <Michiyo.Ravencroft@gmail.com>
2015-10-26 21:12:30 -0500

    Check if event is fired by player.

--- src/main/java/pcl/OpenFM/misc/OFMBreakEvent.java
+++ src/main/java/pcl/OpenFM/misc/OFMBreakEvent.java
@@ -1,6 +1,7 @@
 package pcl.OpenFM.misc;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.event.world.BlockEvent.BreakEvent;
@@ -21,22 +22,24 @@ public class OFMBreakEvent {
 
 	@SubscribeEvent(priority=EventPriority.NORMAL)
 	public void onBlockBreak(BreakEvent event) {
-		if (!IsOp(event.getPlayer())) {
-			TileEntity TE = event.world.getTileEntity(event.x, event.y, event.z);
-			if(TE instanceof TileEntityRadio){
-				TileEntityRadio xEntity = (TileEntityRadio) TE;
-				if(xEntity.owner!=null){
-					if(!xEntity.owner.equals(event.getPlayer().getUniqueID().toString()) && xEntity.isLocked) {
-						if(!xEntity.owner.isEmpty()) {
-							event.setCanceled(true);						
+		if (event.getPlayer() instanceof EntityPlayerMP) {
+			if (!IsOp(event.getPlayer())) {
+				TileEntity TE = event.world.getTileEntity(event.x, event.y, event.z);
+				if(TE instanceof TileEntityRadio){
+					TileEntityRadio xEntity = (TileEntityRadio) TE;
+					if(xEntity.owner!=null){
+						if(!xEntity.owner.equals(event.getPlayer().getUniqueID().toString()) && xEntity.isLocked) {
+							if(!xEntity.owner.isEmpty()) {
+								event.setCanceled(true);						
+							}
 						}
 					}
 				}
-			}
-		} else {
-			TileEntity TE = event.world.getTileEntity(event.x, event.y, event.z);
-			if(TE instanceof TileEntityRadio){
-				OpenFM.logger.info("Op is breaking a radio at X:" + event.x + " Y: " + event.y + " Z: " + event.z);
+			} else {
+				TileEntity TE = event.world.getTileEntity(event.x, event.y, event.z);
+				if(TE instanceof TileEntityRadio){
+					OpenFM.logger.info("Op is breaking a radio at X:" + event.x + " Y: " + event.y + " Z: " + event.z);
+				}
 			}
 		}
 	}
