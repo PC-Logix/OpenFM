24dca69c2ee9f558caed47bb9ab2f7a66c1f15b1
Caitlyn Mainer <michiyo.ravencroft@gmail.com>
2016-09-09 15:23:44 -0500

    Fixes DepLoader

--- src/main/java/pcl/OpenFM/misc/OFMDepLoader.java
+++ src/main/java/pcl/OpenFM/misc/OFMDepLoader.java
@@ -1,39 +1,53 @@
 package pcl.OpenFM.misc;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.JarURLConnection;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.Map;
 
-import net.minecraftforge.fml.relauncher.IFMLCallHook;
-import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import pcl.OpenFM.BuildInfo;
+import cpw.mods.fml.relauncher.IFMLCallHook;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 
 public class OFMDepLoader implements IFMLLoadingPlugin, IFMLCallHook {
-	public void load() {
-		System.out.println("Unpacking Sound plugins to mods directory");
-		File jar = null;
-		File f = new File("mods/");
-			try {
-				jar = new File(OFMDepLoader.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
-			} catch (URISyntaxException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+	public void load() throws IOException, URISyntaxException {
+		System.out.println("Starting OpenFM DepLoader!");
+		File f = new File("mods"+File.separator);
+		f.mkdirs();
+		final String path = "assets/openfm/deps";
+		final File jarFile = new File(getClass().getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
+		if(jarFile.isFile()) {  // Run with JAR file
+			JarFile jar = null;
+			jar = new JarFile(jarFile);
+			final Enumeration<JarEntry> entries = jar.entries(); //gives ALL entries in jar
+			while(entries.hasMoreElements()) {
+				final String name = entries.nextElement().getName();
+				if (name.startsWith(path + "/") && name.endsWith(".jar")) { //filter according to the path
+					InputStream jarStream = getClass().getClassLoader().getResourceAsStream(name);
+					Path p = Paths.get(name);
+					String file = p.getFileName().toString();
+					System.out.println("Extracting file: " + file);
+					try (FileOutputStream fos = new FileOutputStream(f + File.separator + file);){
+						byte[] buf = new byte[2048];
+						int r;
+						while(-1 != (r = jarStream.read(buf))) {
+							fos.write(buf, 0, r);
+						}
+					}
+				}
 			}
-					
-			try {
-				URL url = new URL("jar:file:" + jar + "!/assets/openfm/deps/");
-				JarURLConnection jarConnection = (JarURLConnection)url.openConnection();
-				FileUtils.copyJarResourcesRecursively(f, jarConnection);
-			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			jar.close();
+		} else {
+			System.out.println("ERROR: Can't detect valid JAR");
+		}
 	}
 
 	@Override
@@ -57,7 +71,12 @@ public class OFMDepLoader implements IFMLLoadingPlugin, IFMLCallHook {
 
 	@Override
 	public Void call() {
-		load();
+		try {
+			load();
+		} catch (IOException | URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		return null;
 	}
