e7e90c1ff14241bcc94187f7521f37f293125ea7
gamax92 <gamax92@aol.com>
2017-02-17 10:39:49 -0700

    Convert MP3 and OGG players to use OpenAL based playback
    Should fix all isses on arch/alsa/pulseaudio
    Allows audio to come out of MineCraft's process
    Fixes delayed volume changes
    Fixes laggy stream closing
    
    Should also fix a number of potential issues with closing the stream

--- src/main/java/pcl/OpenFM/player/MP3Player.java
+++ src/main/java/pcl/OpenFM/player/MP3Player.java
@@ -1,6 +1,11 @@
 package pcl.OpenFM.player;
 
 import java.io.InputStream;
+import java.nio.IntBuffer;
+import java.nio.ShortBuffer;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.openal.AL10;
 
 import javazoom.jl.decoder.Bitstream;
 import javazoom.jl.decoder.BitstreamException;
@@ -8,18 +13,18 @@ import javazoom.jl.decoder.Decoder;
 import javazoom.jl.decoder.Header;
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.decoder.SampleBuffer;
-import javazoom.jl.player.AudioDevice;
-import javazoom.jl.player.FactoryRegistry;
 import javazoom.jl.player.advanced.PlaybackEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.audio.SoundCategory;
 import net.minecraft.world.World;
 import pcl.OpenFM.ContentRegistry;
+import pcl.OpenFM.OpenFM;
 
 public class MP3Player {
 	private Bitstream bitstream;
 	private Decoder decoder;
-	private AudioDevice audio;
+	private IntBuffer buffer;
+	private IntBuffer source;
 	private PlaybackListener listener;
 	private float volume = 0.0F;
 	private int posX;
@@ -27,8 +32,12 @@ public class MP3Player {
 	private int posZ;
 	private World world;
 	public InputStream ourStream = null;
+	private boolean playing = false;
+
 	public MP3Player(InputStream stream) throws JavaLayerException {
-		this(stream, null);
+		ourStream = stream;
+		this.bitstream = new Bitstream(stream);
+		this.decoder = new Decoder();
 	}
 
 	public void setID(World w, int x, int y, int z) {
@@ -38,35 +47,43 @@ public class MP3Player {
 		this.world = w;
 	}
 
-	public MP3Player(InputStream stream, AudioDevice device) throws JavaLayerException {
-		ourStream = stream;
-		this.bitstream = new Bitstream(stream);
-		if (device != null)
-		{
-			this.audio = device;
+	private boolean alError() {
+		if (AL10.alGetError() != AL10.AL_NO_ERROR) {
+			OpenFM.logger.error(String.format("AL10 Error: %d: %s", AL10.alGetError(), AL10.alGetString(AL10.alGetError())));
+			return true;
 		}
-		else
-		{
-			this.audio = FactoryRegistry.systemRegistry().createAudioDevice();
-		}
-
-		this.audio.open(this.decoder = new Decoder());
+		return false;
 	}
 
-	public void play() throws JavaLayerException {
-		play(Integer.MAX_VALUE);
+	public boolean play() throws JavaLayerException {
+		return play(Integer.MAX_VALUE);
 	}
 
 	public boolean play(int frames) throws JavaLayerException {
 		boolean ret = true;
 
+		this.source = BufferUtils.createIntBuffer(1);
+		AL10.alGenSources(this.source);
+		if (alError()) {
+			close();
+			return false;
+		}
 
+		AL10.alSourcei(this.source.get(0), AL10.AL_LOOPING, AL10.AL_FALSE);
+		AL10.alSourcef(this.source.get(0), AL10.AL_PITCH, 1.0f);
+		AL10.alSourcef(this.source.get(0), AL10.AL_GAIN, this.volume * Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.RECORDS));
+		if (alError()) {
+			close();
+			return false;
+		}
+
+		this.playing = true;
 		if (this.listener != null)
 		{
 			this.listener.playbackStarted(createEvent(PlaybackEvent.STARTED));
 		}
 
-		while ((frames-- > 0) && (ret))
+		while ((this.playing) && (ret))
 		{
 			if (!ContentRegistry.checkBlock(this.world, this.posX, this.posY, this.posZ)) {
 				close();
@@ -74,80 +91,77 @@ public class MP3Player {
 			ret = decodeFrame();
 		}
 
-		AudioDevice out = this.audio;
-
-		if (out != null)
-		{
-			out.flush();
-			synchronized (this)
-			{
-				close();
-			}
-			if (this.listener != null)
-			{
-				this.listener.playbackFinished(createEvent(out, PlaybackEvent.STOPPED));
+		if (this.playing) {
+			while (AL10.alGetSourcei(this.source.get(0), AL10.AL_SOURCE_STATE) == AL10.AL_PLAYING) {
+				try {
+					Thread.sleep(1);
+				} catch (InterruptedException e) {}
 			}
 		}
+
+		close();
+
 		return ret;
 	}
 
-	public synchronized void close() {
-		AudioDevice out = this.audio;
-
-		if (out != null)
+	private void close() {
+		this.playing = false;
+		if (this.listener != null)
 		{
-			this.audio = null;
-			out.close();
-			out.getPosition();
-			try
-			{
-				this.bitstream.close();
-			}
-			catch (BitstreamException localBitstreamException) {}
+			this.listener.playbackFinished(createEvent(PlaybackEvent.STOPPED));
 		}
+		if (this.source != null) {
+			AL10.alSourceStop(this.source);
+			AL10.alDeleteSources(this.source);
+			this.source = null;
+		}
+		if (this.buffer != null) {
+			AL10.alDeleteBuffers(this.buffer);
+			this.buffer = null;
+		}
+		try
+		{
+			this.bitstream.close();
+		}
+		catch (BitstreamException localBitstreamException) {}
 	}
 
 	protected boolean decodeFrame() throws JavaLayerException {
 		try
 		{
-			AudioDevice out = this.audio;
-
-			if (out == null)
-			{
-				return false;
-			}
-
 			Header h = this.bitstream.readFrame();
-
 			if (h == null)
 			{
 				return false;
 			}
 
-
 			SampleBuffer output = (SampleBuffer)this.decoder.decodeFrame(h, this.bitstream);
+			short[] samples = output.getBuffer();
 
-			synchronized (this)
-			{
-				out = this.audio;
-				
-				if (out != null)
-				{
-					short[] samples = output.getBuffer();
-
-					for (int samp = 0; samp < samples.length; samp++)
-					{
-						samples[samp] = ((short)(int)(samples[samp] * this.volume * ((Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.RECORDS) * Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.MASTER)))));
-					}					
-					out.write(samples, 0, output.getBufferLength());
+			if (this.buffer == null) {
+				this.buffer = BufferUtils.createIntBuffer(1);
+			} else {
+				int processed = AL10.alGetSourcei(this.source.get(0), AL10.AL_BUFFERS_PROCESSED);
+				if (processed > 0) {
+					AL10.alSourceUnqueueBuffers(this.source.get(0), this.buffer);
 				}
+			}
+
+			AL10.alGenBuffers(this.buffer);
+			ShortBuffer data = (ShortBuffer) BufferUtils.createShortBuffer(output.getBufferLength()).put(samples, 0, output.getBufferLength()).flip();
+			AL10.alBufferData(this.buffer.get(0), (output.getChannelCount() > 1) ? AL10.AL_FORMAT_STEREO16 : AL10.AL_FORMAT_MONO16, data, output.getSampleFrequency());
+			AL10.alSourceQueueBuffers(this.source.get(0), buffer);
+
+			int state = AL10.alGetSourcei(this.source.get(0), AL10.AL_SOURCE_STATE);
+			if (this.playing && state != AL10.AL_PLAYING) {
+				AL10.alSourcePlay(this.source.get(0));
 			}
 
 			this.bitstream.closeFrame();
 		}
 		catch (RuntimeException ex)
 		{
-			stop();
+			close();
 			//play();
 			throw new JavaLayerException("Exception decoding audio frame", ex);
 		}
@@ -178,11 +192,7 @@ public class MP3Player {
 	}
 
 	private PlayBackEvent createEvent(int id) {
-		return createEvent(this.audio, id);
-	}
-
-	private PlayBackEvent createEvent(AudioDevice dev, int id) {
-		return new PlayBackEvent(this, id, dev.getPosition());
+		return new PlayBackEvent(this, id, 0);
 	}
 
 	public void setPlayBackListener(PlaybackListener listener) {
@@ -194,12 +204,18 @@ public class MP3Player {
 	}
 
 	public void stop() {
-		this.listener.playbackFinished(createEvent(PlayBackEvent.STOPPED));
-		close();
+		this.playing = false;
+		if (this.source != null) {
+			AL10.alSourcef(this.source.get(0), AL10.AL_GAIN, 0.0f);
+			AL10.alSourceStop(this.source);
+		}
 	}
 
 	public void setVolume(float f) {
 		this.volume = f;
+		if (this.playing && this.source != null) {
+			AL10.alSourcef(this.source.get(0), AL10.AL_GAIN, f * Minecraft.getMinecraft().gameSettings.getSoundLevel(SoundCategory.RECORDS));
+		}
 	}
 
 	public float getVolume() {
